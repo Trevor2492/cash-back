a6d504f14386bc7abbf66dc3121dd9f9186c7424
Trevor <trevor2492@gmail.com>
2021-02-09 16:57:04 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="cash-back">
+        <div>20<sup className="percent">%</sup></div>
+        <div className="cash-back2">
+          <div>CASH</div>
+          <div>BACK</div>
+        </div>
+      </div>
+
+      <div className="join">On all purchases when you join our membership</div><br />
+
+      <div className="and">
+        <hr></hr>
+        <div>AND</div>
+        <hr></hr>
+      </div><br />
+
+      <div className="membership">
+        <img src="https://via.placeholder.com/250?text=Gift+Card+Image" alt=""></img>
+        <div className="month">Get the cost of your membership back as a $35 Gift Card each month</div>
+      </div>
+      
+      <div className="lower">
+        <div className="how-to-setup">
+          How to Setup
+            <div className="setup">
+              <div className="list">
+                <img src="https://via.placeholder.com/50x50" alt=""></img>
+                <div>Download the Breeze app and sign up</div>
+              </div>
+              <div className="list">
+                <img src="https://via.placeholder.com/50x50" alt=""></img>
+                <div>Join our membership and connect <br />your bank account</div>
+              </div>
+              <div className="list">
+                <img src="https://via.placeholder.com/50x50" alt=""></img>
+                <div>Scan to make a payment, earn cashback!</div>
+              </div>
+            </div>
+            <div className="search">
+              Search "Breeze Memberships" to download
+              <div className="icons">
+                <img src="https://via.placeholder.com/200x50?text=Google+Play+Logo" alt=""></img>
+                <img src="https://via.placeholder.com/200x50?text=App+Store+Logo" alt=""></img>
+              </div>
+            </div>
+        </div>
+        <img src="https://via.placeholder.com/250x400?text=iPhone+Image" alt=""></img>
+      </div>
+
     </div>
   );
 }
